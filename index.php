4cb504c9bfc0b58ce805a611f68f1c6caed896dc
Priyesh Patel <priyesh@ukawala.com>
2011-04-20 11:10:07 +0100

    config integrated

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+require 'Envato_marketplaces.php';
+require 'config.php';
+$e = new Envato_marketplaces();
+$e->set_api_key($config['api_key']);
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml"> 
+
+<!-- Begin Head -->
+<head>
+    
+    <!-- Page Title -->
+    <title>Envato Marketplace Purchase Verifier</title>
+    
+</head>
+<!-- End Head -->
+
+<!-- Begin Body -->
+<body>
+
+</body>
+<!-- End Body -->
+
+</html>
+
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 ## Based on Jeffrey Way's Marketplace API Wrapper Class
 
 This class provides a form to quickly verify purchases from buyers who request support. 
+Make sure to configure the script by changing config.php.
